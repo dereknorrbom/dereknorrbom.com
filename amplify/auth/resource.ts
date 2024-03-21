3544f9bef80fe824f2f6b824d96ef1f3f93928bb
Derek Norrbom <dereknorrbom@gmail.com>
2024-03-21 01:32:55 -0700

    auth
    auth

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -6,6 +6,9 @@ import { defineAuth } from '@aws-amplify/backend';
  */
 export const auth = defineAuth({
   loginWith: {
-    email: true,
+    email: {
+      verificationEmailSubject: 'Welcome! Verify your email!',
+      verificationEmailStyle: "LINK",
+      verificationEmailBody: (link: string) => `Your verification link is ${link}.`},
   },
 });
